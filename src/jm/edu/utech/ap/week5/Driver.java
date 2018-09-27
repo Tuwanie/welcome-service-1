3921972d2e7a60a8da589835168bb09508870120
Tuwanie <tuwaniemorgan@outlook.com>
2018-09-27 14:22:40 -0500

    Added AppEngine, Driveable, Driver

---
+++ src/jm/edu/utech/ap/week5/Driver.java
@@ -0,0 +1,9 @@
+package jm.edu.utech.ap.week5;
+
+public class Driver implements Driveable{
+
+	public void drive (float numberOfMiles){
+		System.out.println(String.format("Tuwanie has driven " +"%.2f miles", numberOfMiles));
+	}
+	
+}
